8f6fce3570e609a751c66b8c4974c824919380ad
honc.z <475050973@qq.com>
2018-10-17 17:09:30 +0800

    增加模块

---
+++ zrpc-test/zrpc-test-server/src/main/java/com/honcz/server/zrpcserver/ZrpcServerApplication.java
@@ -0,0 +1,12 @@
+package com.honcz.server.zrpcserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZrpcServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZrpcServerApplication.class, args);
+    }
+}
