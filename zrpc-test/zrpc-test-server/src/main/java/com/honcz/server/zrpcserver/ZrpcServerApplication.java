3a13e81dfc320ae90e9399276f8533b6d23ec34e
honc.z <475050973@qq.com>
2019-05-22 16:50:47 +0800

    统一版本管理

--- zrpc-test/zrpc-test-server/src/main/java/com/honcz/server/zrpcserver/ZrpcServerApplication.java
+++ zrpc-test/zrpc-test-server/src/main/java/com/honcz/server/zrpcserver/ZrpcServerApplication.java
@@ -1,6 +1,5 @@
 package com.honcz.server.zrpcserver;
 
-import com.honcz.zrpc.zrpcclient.annotation.EnableZRPCClients;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
